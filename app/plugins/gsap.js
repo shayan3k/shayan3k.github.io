405a32e43de31b016af5767fd053ff1fc8adba7b
shayan <shayan3k@gmail.com>
2024-04-09 18:55:45 +1000

    update resume with work exp

--- app/plugins/gsap.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-import { gsap } from "gsap";
-
-Object.defineProperty(Vue.prototype, "$gsap", { value: gsap });
--- app/plugins/vue-awesome-swiper.js
+++
@@ -1,7 +0,0 @@
-import Vue from "vue";
-import VueAwesomeSwiper from "vue-awesome-swiper";
-
-// import custom style
-import "../node_modules/swiper/swiper.min.css";
-
-Vue.use(VueAwesomeSwiper);
